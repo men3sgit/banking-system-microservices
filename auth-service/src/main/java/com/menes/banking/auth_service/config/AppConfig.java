fc6790df426d6d9a29d42ee9f5222bf1fb8faf54
Men Duong <menesforenglish102@gmail.com>
2025-08-07 21:43:34 +0700

    fix(kafka): ensure LocalDateTime serialized as epoch millis using custom ObjectMapper

--- auth-service/src/main/java/com/menes/banking/auth_service/config/AppConfig.java
+++ auth-service/src/main/java/com/menes/banking/auth_service/config/AppConfig.java
@@ -1,11 +1,18 @@
 package com.menes.banking.auth_service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.menes.banking.auth_service.config.serialize.LocalDateTimeSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,4 +30,6 @@ public class AppConfig {
         log.info("Starting new fixed thread pool");
         return Executors.newFixedThreadPool(2);
     }
+
+
 }
